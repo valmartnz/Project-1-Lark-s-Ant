40732440994e3469a1662adb866713de9b137b0f
Jason Duong <76750523+duong-jason@users.noreply.github.com>
2021-09-03 16:11:57 -0700

    :)

--- assets/board.js
+++ assets/board.js
@@ -1,3 +1,14 @@
-function board() {
+/*
+  Authors:
+    Valerie Martinez
+    Anthony Sanchez
+    Bradley Diep
+    Jason Duong
+
+  Description:
+    draws the canvas onto website
+*/
+
+function board(rminor, rmajor, rstroke, rfill) {
 
 }
