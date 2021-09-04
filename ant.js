550e3e65d3c629285ba9d9cfe718751aa943d9d4
duong-jason <duongjason2000@gmail.com>
2021-09-03 17:40:11 -0700

    can see ant on board now

--- ant.js
+++ ant.js
@@ -10,11 +10,16 @@
 */
 
 var ant_canvas = { cell: 10, width: 60, height: 40 };
+var ant_color = ['#000000', '#89CFF0', '#FFF300', '#FF6347']; // array of colors { black, blue, yellow, red }
+var ant_bot = { x: 300, y: 200, face: 0, color: ant_color[1] }; // start pos = (30, 20) | 0 = north
 
 function setup() {
   createCanvas(ant_canvas.cell * ant_canvas.width, ant_canvas.cell * ant_canvas.height);
+  draw();
 }
 
 function draw() {
-  // put drawing code here
+  // testing
+  rect(ant_bot.x, ant_bot.y, ant_canvas.cell, ant_canvas.cell);
+  fill(ant_bot.color);
 }
